7f9a04b8dc1c028d588a47121ac43bbe06503652
elfadili-ae <elfadili.ae@gmail.com>
2024-01-29 20:35:25 +0100

    changed heading

--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -4,5 +4,5 @@ import './header.css';
 $('body').append('<header></header>')
 
 $('header').append("<div id='logo'></div>");
-$('header').append('<H1>Holberton Dashboard</H1>');
+$('header').append('<h1>Holberton Dashboard</h1>');
 console.log('Init header');
