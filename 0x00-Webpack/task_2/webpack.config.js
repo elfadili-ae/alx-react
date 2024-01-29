44ca2f7b6254d493b7d972c1dabe95e876879851
elfadili-ae <elfadili.ae@gmail.com>
2024-01-29 20:39:44 +0100

    Fix: image handling

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -16,30 +16,14 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.(gif|png|jpe?g|svg)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 use: [
                     'file-loader',
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            mozjpeg: {
-                                progressive: true,
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.9],
-                                speed: 4,
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75,
-                            },
+                            bypassOnDebug: true,
+                            disable: true,
                         },
                     },
                 ],
