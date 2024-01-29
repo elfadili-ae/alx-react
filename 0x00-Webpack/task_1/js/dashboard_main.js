5cc07a4426498cd49eb304a4a6e5df4d235887f0
elfadili-ae <elfadili.ae@gmail.com>
2024-01-29 14:16:15 +0100

    webpack config

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+let counter = 0;
+
+function updateCounter() {
+    counter += 1;
+    $('#count').html(`${counter} clicks on the button`);
+}
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
+
+$('button').on('click', _.debounce(updateCounter, 500));
