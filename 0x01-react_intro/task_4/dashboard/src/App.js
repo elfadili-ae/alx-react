ee6b1d36b742ce933b117d490985b7e9b270917c
elfadili-ae <elfadili.ae@gmail.com>
2024-01-31 12:49:33 +0100

    gh page

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email:</label>
+        <input id='email' type='email' />
+        <label htmlFor='password'>Password:</label>
+        <input id='password' type='password' />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
