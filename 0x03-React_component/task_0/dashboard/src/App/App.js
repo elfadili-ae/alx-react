ed0c0727f430f0f43f6b6de4d97adf1d32a64705
elfadili-ae <elfadili.ae@gmail.com>
2024-02-06 11:06:16 +0100

    react componenet

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Notif from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import propTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
+
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 }
+]
+
+const listNotif = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() }
+]
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Notif listNotifications={listNotif} />
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            {this.props.isLoggedIn ? (
+              <CourseList listCourses={listCourses} />
+            ) : (
+              <Login />
+            )
+            }
+          </div>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: propTypes.bool
+}
+
+App.defaultProps = {
+  isLoggedIn: false
+}
+
+export default App;
