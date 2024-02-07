44279668ab0b947add004c120691832b39f03e87
elfadili-ae <elfadili.ae@gmail.com>
2024-02-07 11:05:43 +0100

    binded variable

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,35 +1,50 @@
-import React from 'react';
-import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Notif from '../Notifications/Notifications';
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
-import propTypes from 'prop-types';
-import { getLatestNotification } from '../utils/utils';
-
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notif from "../Notifications/Notifications";
+import "./App.css";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.keyressHandler = this.keyressHandler.bind(this);
   }
 
   listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 }
-  ]
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
 
-  listNotif = [
+  listNotifications = [
     { id: 1, type: "default", value: "New course available" },
     { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: getLatestNotification() }
-  ]
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
+  keyressHandler(e) {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyressHandler);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyressHandler);
+  }
 
   render() {
     return (
       <React.Fragment>
-        <Notif listNotifications={this.listNotif} />
+        <Notif listNotifications={this.listNotifications} />
         <div className="App">
           <Header />
           <div className="App-body">
@@ -45,32 +60,18 @@ class App extends React.Component {
       </React.Fragment>
     );
   }
-
-  keypressedHandler(envent) {
-    if (event.ctrlKey && event.key == 'h') {
-      alert('Logging you out');
-      this.props.log
-    }
-  }
-  componentDidMount() {
-    document.addEventListener('keydown', this.keypressedHandler)
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.keypressedHandler)
-  }
-}
-
-App.propTypes = {
-  isLoggedIn: propTypes.bool,
-  logOut: propTypes.func
 }
 
 App.defaultProps = {
   isLoggedIn: false,
   logOut: () => {
     return;
-  }
-}
+  },
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
 
 export default App;
