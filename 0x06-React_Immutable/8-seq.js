3def5103c775030a07b4508c88000445f3dccf13
elfadili-ae <elfadili.ae@gmail.com>
2024-02-22 11:47:06 +0000

    seq

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,21 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents(object) {
+  function capLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const filtered = Seq(object).filter((student) => {
+    return student.score > 70;
+  });
+
+  const newObj = filtered.toJS();
+
+  Object.keys(newObj).map((key) => {
+    newObj[key].firstName = capLetter(newObj[key].firstName);
+    newObj[key].lastName = capLetter(newObj[key].lastName);
+    return newObj[key];
+  });
+
+  console.log(newObj);
+}
