b548a766857609ceafca84696fc8266e4799123f
elfadili-ae <elfadili.ae@gmail.com>
2024-03-08 13:01:56 +0100

    connect user state to the footer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { getFullYear, getFooterCopy } from '../utils/utils';
 import './Footer.css'
-import { AppContext } from '../App/AppContext'
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
 
-const Footer = () => {
-    const { user } = React.useContext(AppContext);
+const Footer = ({ user }) => {
+    // const { user } = React.useContext(AppContext);
 
     return (
         <div className="App-footer">
             <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-            {user.isLoggedIn && (
+            {user && (
                 <p><a href='#'>Contact us</a></p>
             )}
         </div>
     )
 }
 
-export default Footer;
+Footer.defaultProps = {
+    user: null
+}
+
+Footer.propTypes = {
+    user: propTypes.object,
+}
+
+// export default Footer;
+
+export const mapStateToProps = (state) => {
+    return {
+        user: state.get('user'),
+    }
+}
+
+export default connect(mapStateToProps, null)(Footer);
