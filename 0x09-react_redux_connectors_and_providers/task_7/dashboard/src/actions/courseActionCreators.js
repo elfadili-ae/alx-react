0c8896657fdefc23fcc5eb1872fb547c249fbd7d
elfadili-ae <elfadili.ae@gmail.com>
2024-03-09 14:12:40 +0100

    fetch courses function

--- 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
@@ -1,4 +1,4 @@
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { SELECT_COURSE, UNSELECT_COURSE, FETCH_COURSE_SUCCESS } from "./courseActionTypes";
 
 export const selectCourse = (index) => {
     return {
@@ -17,3 +17,19 @@ export const unSelectCourse = (index) => {
 }
 
 export const boundUnselectCourse = (index) => { dispatch(unSelectCourse(index)) };
+
+export const setCourses = (data) => {
+    return {
+        type: FETCH_COURSE_SUCCESS,
+        data,
+    };
+};
+
+export const fetchCourses = () => {
+    return (dispatch) => {
+        return fetch("./courses.json")
+            .then((res) => res.json())
+            .then((data) => dispatch(setCourses(data)))
+            .catch((error) => { });
+    };
+};
